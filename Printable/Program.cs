8f051b2fa1e1c863f1ea894501c2d00f0dacd719
Даниил Пронин <daniilpronin250505@mail.ru>
2023-02-07 23:03:21 +0300

    Добавьте файлы проекта.

---
+++ Printable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+public interface Printable
+{
+    public void Print();
+}
+public class Book : Printable
+{
+    public void Print()
+    {
+        static void printBooks(Printable[] printable)
+        {
+            Console.WriteLine(printable);
+        }
+    }
+}
+public class Magazine : Printable
+{
+    public void Print() 
+    {
+        static void printMagazines(Printable[] printable)
+        {
+            Console.WriteLine(printable);
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book kniga = new Book();
+        Magazine zhyrnal = new Magazine();
+        Printable[] masiv = { };
+        foreach (Printable i in masiv)    
+        {
+            kniga.Print();
+            zhyrnal.Print();
+        }
+    }
+}
+
+
